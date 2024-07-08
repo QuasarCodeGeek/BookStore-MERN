bea158f283dd54d421f33845df2fac68ad1123cd
Jayson Reyes <jaysonreyes.jsr11@gmail.com>
2024-07-08 08:26:27 +0800

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+// app.user(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowHeaders: ['Content-Type'],
+    })
+);
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN stack tutorial');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('App connected to database');
+
+    app.listen(PORT, () => {
+        console.log(`App is listening ton port: ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.log(error);
+})
